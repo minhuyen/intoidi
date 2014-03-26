1a913db36abe1963a6aa9b9cca8bed1e21d5d068
Radek Svarz <radek.svarz@gmail.com>
2014-03-26 22:35:03 +0000

    setup.py edited online with Bitbucket

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     name='Mezzanine on Openshift',
     version='0.1',
     description='Mezzanine on Openshift',
-    author='',
+    author='Radek Svarz',
     author_email='',
     url='',
     install_requires=[
-        'Django==1.5.8',
+        'Django==1.6.2',
         'mezzanine==3.0.9',
         'django_compressor==1.3',
         'South==0.8.4',
@@ -19,6 +19,7 @@ setup(
         'docutils',
         'django-reversion',
         'django-adminactions',
+        'django-smuggler',
     ],
 )
 
