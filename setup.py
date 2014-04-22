62411e38f7390dcd12a27bc8d3a8d72bbe6ac882
radeksvarz <radek.svarz@gmail.com>
2014-04-22 23:09:58 +0200

    update Mezzanine to 3.1.1

--- setup.py
+++ setup.py
@@ -4,23 +4,24 @@ from setuptools import setup
 
 setup(
     name='Mezzanine on Openshift',
-    version='0.1',
+    version='0.2',
     description='Mezzanine on Openshift',
     author='Radek Svarz',
     author_email='',
     url='',
     install_requires=[
-        'Django==1.6.2',
-        'psycopg2==2.5.2',
-        'mezzanine==3.0.9',
+        'Django==1.6.3',
+        'psycopg2==2.5.2', # important as Openshift default is the older 2.0.4 version throwing errors
+        'mezzanine==3.1.1',
         'django_compressor==1.3',
         'South==0.8.4',
-        'django-tagging',
+        # requirements bellow are optional
+        'django-tagging==0.3.2',
         # 'django-debug-toolbar',  - throughs url errors on Openshift!
-        'docutils',
-        'django-reversion',
-        'django-adminactions',
-        'django-smuggler',
+        'docutils==0.11',
+        'django-reversion==1.8.0',
+        'django-adminactions==0.4',
+        'django-smuggler==0.4.1',
     ],
 )
 
