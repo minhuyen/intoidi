78cf742f2901fa8f2a8c2b709a4dcf071b2033ba
Radek Svarz <radek.svarz@gmail.com>
2014-07-01 13:51:57 +0000

    updated South dependency

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'psycopg2==2.5.3', # important as Openshift default is the older 2.0.4 version throwing errors
         'mezzanine==3.1.5',
         'django_compressor==1.4',
-        'South==0.8.4',
+        'South==1.0',
         ##################################### Requirements bellow are optional ###########################
         'django-tagging==0.3.2',
         # 'django-debug-toolbar',  - throws url errors on Openshift!
