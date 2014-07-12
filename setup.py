2b7b7f37aef5ec8c66fb1e1e9bf5c98992c45a21
Radek Svarz <radek.svarz@gmail.com>
2014-07-12 07:40:07 +0000

    mezz 319

--- setup.py
+++ setup.py
@@ -10,19 +10,22 @@ setup(
     author_email='',
     url='https://bitbucket.org/radeksvarz/mezzanineopenshift',
     install_requires=[
-        'Django==1.6.5',
+        'Django==1.6.5',             # 
         'psycopg2==2.5.3',           # important as Openshift default is the older 2.0.4 version throwing errors
-        'mezzanine==3.1.8',
-        'django_compressor==1.4',
-        'South==1.0',
-        ##################################### Requirements bellow are optional ###########################
-        'django-tagging==0.3.2',     # Tagging feature for Django models
+        'mezzanine==3.1.9',          # Mezzanine itself
+        'django_compressor==1.4',    # Compresses linked, inline Javascript, CSS in a template into cacheable static files
+        'South==1.0',                # Intelligent database migrations library for the Django web framework.
+
+        ##################################### Libraries bellow are tested as not working ###########################
         # 'django-debug-toolbar',    # !!! - throws url errors on Openshift! Do not use !
+        
+        ##################################### Requirements bellow are working and optional ###########################
+        'django-tagging==0.3.2',     # Tagging feature for Django models
         'docutils==0.12',            # Autodocumentation in admin 
         'django-reversion==1.8.1',   # Historical versions of records in admin
         'django-adminactions==0.5',  # mass admin import / export / graph actions in the admin lists
         'django-smuggler==0.4.1',    # data load / dump in json from the admin URL
-        'mezzanine-page-auth==0.3.1',#
+        'mezzanine-page-auth==0.3.1',# group-level permission to pages
         'django-countries==2.1.2',   # Countries model field incl. combo box implementation
         'Whoosh==2.6.0',             # search in pure python 
         'raven==5.0.0',              # for Sentry integration - see getsentry.com
