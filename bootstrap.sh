a72117b401d5a2e3f03c163511f030b0587f391b
nguyenbanh <tonny89banh@gmail.com>
2014-11-23 11:27:46 +0700

    Setup evironment #1

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 echo "Install and dependencies"
-#sudo apt-get update
+sudo apt-get update
 sudo apt-get install -y python-dev libpq-dev build-essential libssl-dev libffi-dev
 
 echo "Install postgresql"
-#sudo apt-get install -y postgresql python-psycopg2
+sudo apt-get install -y postgresql python-psycopg2
 
 echo "Install mercurial and git"
-#sudo apt-get install -y mercurial git
+sudo apt-get install -y mercurial git
 
 echo "Install pip"
-#sudo apt-get install -y python-pip
+sudo apt-get install -y python-pip
 
 echo "Install virtual environment"
-#sudo pip install virtualenv
+sudo pip install virtualenv
 cd /vagrant
 virtualenv venv
 
