405213f2d24ef067e781ec250fae19277032d968
shin <shinbutchi@ymail.com>
2015-02-06 15:51:58 +0700

    Add pagination to product page #10

--- wsgi/shopping/page_processors.py
+++ wsgi/shopping/page_processors.py
@@ -1,13 +1,26 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from mezzanine.pages.page_processors import processor_for
 from wsgi.shopping.models import HomePage, ProductsPage, Product, ProductImage
 
 
 @processor_for(ProductsPage)
 def products_processor(request, page):
-    product_list = Product.objects.all().order_by("updated_date").reverse()
     product_image_list = []
-    for product in product_list:
+    products = []
+
+    product_list = Product.objects.all().order_by("updated_date").reverse()
+    paginator = Paginator(product_list, 4)
+
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    for product in products:
         productImage = ProductImage.objects.get(product_id=product.id)
         product_image_list.append(productImage.image)
-    return {"product_list": product_list, "product_image_list": product_image_list}
+    return {"products": products, "product_image_list": product_image_list}
 
--- wsgi/shopping/templatetags/shopping_filters.py
+++ wsgi/shopping/templatetags/shopping_filters.py
@@ -2,6 +2,12 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def get_at_index(list, index):
     return list[index]
+
+
+@register.filter
+def get_range(value):
+    return [i + 1 for i in range(value)]
