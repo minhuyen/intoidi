a02a8af2e955390377eb279da6714b5eec42e514
shin <shinbutchi@ymail.com>
2015-02-06 14:50:55 +0700

    Create dynamic product page #9

--- wsgi/shopping/page_processors.py
+++ wsgi/shopping/page_processors.py
@@ -4,12 +4,10 @@ from wsgi.shopping.models import HomePage, ProductsPage, Product, ProductImage
 
 @processor_for(ProductsPage)
 def products_processor(request, page):
-    product_list = Product.objects.all().order_by("updated_date")
+    product_list = Product.objects.all().order_by("updated_date").reverse()
     product_image_list = []
     for product in product_list:
-
         productImage = ProductImage.objects.get(product_id=product.id)
         product_image_list.append(productImage.image)
-    # product_image_list = ProductImage.objects.all()
     return {"product_list": product_list, "product_image_list": product_image_list}
 
