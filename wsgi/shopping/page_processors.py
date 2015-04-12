3e137c8b398c051f1836360ea2f4cf8ddca4758b
Do Minh Uyen <minhuyendo@gmail.com>
2015-04-12 17:59:17 +0700

    chỉnh sửa template trang product.html -> category.html product_detail.html -> shop/product.html

--- wsgi/shopping/page_processors.py
+++ wsgi/shopping/page_processors.py
@@ -1,4 +1,4 @@
-from cartridge.shop.models import Sale
+from cartridge.shop.models import Sale, Category, ProductOption
 from mezzanine.pages.page_processors import processor_for
 from shopping.models import HomePage, MySale
 
@@ -13,4 +13,11 @@ def deal_of_week(request, page):
             list_products.extend(deal.all_products())
     context = {"deals": list_products}
     print "context: ", context
+    return context
+
+@processor_for(Category)
+def get_product_option_color(request, page):
+    color = 2
+    color_list = ProductOption.objects.filter(type=color)
+    context = {"color_list": color_list}
     return context
